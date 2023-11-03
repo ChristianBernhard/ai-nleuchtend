7fcaeeabb486990a1c6fb61ed6d87080bcd97227
ChristianBernhard <christian.bernhard97@web.de>
2023-11-03 17:35:43 +0100

    fix mobile navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -98,7 +98,7 @@ export function Header() {
         <nav className="relative z-50 flex justify-between">
           <div className="flex items-center md:gap-x-12">
             <Link href="#" aria-label="Home">
-                            <Logo src="/Logo.svg" alt="Logo" className="h-9 w-auto" />
+                            <Logo src="/Logo.svg" alt="Logo" className="h-10 w-auto" />
             </Link>
             <div className="hidden md:flex md:gap-x-6">
               <NavLink href="#about-us">Über uns</NavLink>
@@ -106,7 +106,7 @@ export function Header() {
               <NavLink href="#contact">Kontakt</NavLink>
             </div>
           </div>
-          {/*<div className="flex items-center gap-x-5 md:gap-x-8">*/}
+          <div className="flex items-center gap-x-5 md:gap-x-8">
           {/*  <div className="hidden md:block">*/}
           {/*    <NavLink href="/login">Sign in</NavLink>*/}
           {/*  </div>*/}
@@ -115,10 +115,10 @@ export function Header() {
           {/*      Get started <span className="hidden lg:inline">today</span>*/}
           {/*    </span>*/}
           {/*  </Button>*/}
-          {/*  <div className="-mr-1 md:hidden">*/}
-          {/*    <MobileNavigation />*/}
-          {/*  </div>*/}
-          {/*</div>*/}
+            <div className="-mr-1 md:hidden">
+              <MobileNavigation />
+            </div>
+          </div>
         </nav>
       </Container>
     </header>
